38450760c134381b711e333d3cb9f4574c648b36
jorge <jorge@LAPTOP-D8GDLQRL>
2026-05-09 21:35:36 -0400

    Crea repositorio del modelo en la capa dominio

---
+++ app/src/main/java/edu/ucne/registroocupaciones/domain/ocupaciones/repository/OcupacionRepository.kt
@@ -0,0 +1,12 @@
+package edu.ucne.registroocupaciones.domain.ocupaciones.repository
+
+import edu.ucne.registroocupaciones.domain.ocupaciones.model.Ocupacion
+import kotlinx.coroutines.flow.Flow
+
+interface OcupacionRepository {
+    fun observeOcupaciones(): Flow<List<Ocupacion>>
+    suspend fun getOcupacion(id: Int): Ocupacion?
+    suspend fun upsert(task: Ocupacion): Int
+    suspend fun delete(id: Int)
+    suspend fun exists(id: Int): Boolean
+}
