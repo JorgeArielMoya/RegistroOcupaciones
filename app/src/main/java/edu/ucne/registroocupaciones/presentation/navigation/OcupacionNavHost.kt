90665c5d5555256ddc631ec85438a7463a2b5bb8
jorge <jorge@LAPTOP-D8GDLQRL>
2026-05-09 23:06:47 -0400

    Trabaja la navegacion de la app entre las distintas pantallas

--- app/src/main/java/edu/ucne/registroocupaciones/presentation/navigation/OcupacionNavHost.kt
+++ app/src/main/java/edu/ucne/registroocupaciones/presentation/navigation/OcupacionNavHost.kt
@@ -1,1 +1,42 @@
 package edu.ucne.registroocupaciones.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import edu.ucne.registroocupaciones.presentation.ocupaciones.edit.OcupacionFormScreen
+import edu.ucne.registroocupaciones.presentation.ocupaciones.list.OcupacionListScreen
+
+@Composable
+fun OcupacionNavHost(
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.OcupacionList
+    ) {
+        composable<Screen.OcupacionList> {
+            OcupacionListScreen(
+                onAddOcupacion = {
+                    navController.navigate(Screen.OcupacionForm(0))
+                },
+                onNavigateToEdit = { id ->
+                    navController.navigate(Screen.OcupacionForm(id))
+                }
+            )
+        }
+
+        composable<Screen.OcupacionForm> {
+            OcupacionFormScreen(
+                onBack = {
+                    navController.navigate(Screen.OcupacionList) {
+                        popUpTo(Screen.OcupacionList) {
+                            inclusive = true
+                        }
+                    }
+                }
+            )
+        }
+    }
+}
