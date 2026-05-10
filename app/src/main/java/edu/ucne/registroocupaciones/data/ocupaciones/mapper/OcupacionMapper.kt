a628474d36316310e6f10e3cc283d388b961fd78
jorge <jorge@LAPTOP-D8GDLQRL>
2026-05-09 21:58:59 -0400

    Trabaja todomain y toEntity para comunicar las capas

--- app/src/main/java/edu/ucne/registroocupaciones/data/ocupaciones/mapper/OcupacionMapper.kt
+++ app/src/main/java/edu/ucne/registroocupaciones/data/ocupaciones/mapper/OcupacionMapper.kt
@@ -1,2 +1,16 @@
 package edu.ucne.registroocupaciones.data.ocupaciones.mapper
 
+import edu.ucne.registroocupaciones.data.ocupaciones.local.OcupacionEntity
+import edu.ucne.registroocupaciones.domain.ocupaciones.model.Ocupacion
+
+fun OcupacionEntity.toDomain(): Ocupacion = Ocupacion(
+    ocupacionId = ocupacionId,
+    descripcion = descripcion,
+    sueldo = sueldo
+)
+
+fun Ocupacion.toEntity(): OcupacionEntity = OcupacionEntity(
+    ocupacionId = ocupacionId,
+    descripcion = descripcion,
+    sueldo = sueldo
+)
