42db6cc1907e4167cd1fa2560241a1be1c76d836
jorge <jorge@LAPTOP-D8GDLQRL>
2026-05-13 13:11:33 -0400

    Cambia el nombre del parametro a ocupacion en el upsert

--- app/src/main/java/edu/ucne/registroocupaciones/domain/ocupaciones/repository/OcupacionRepository.kt
+++ app/src/main/java/edu/ucne/registroocupaciones/domain/ocupaciones/repository/OcupacionRepository.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.flow.Flow
 interface OcupacionRepository {
     fun observeOcupaciones(): Flow<List<Ocupacion>>
     suspend fun getOcupacion(id: Int): Ocupacion?
-    suspend fun upsert(task: Ocupacion): Int
+    suspend fun upsert(ocupacion: Ocupacion): Int
     suspend fun delete(id: Int)
     suspend fun existsByDescripcion(descripcion: String, excludeId: Int = 0): Boolean
 }
