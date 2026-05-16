a4ace62da6eb2356dd4947ec09b7f74644810c22
jorge <jorge@LAPTOP-D8GDLQRL>
2026-05-15 23:02:27 -0400

    Corrige el error de convertir empleado a empleado

--- app/src/main/java/edu/ucne/registroocupaciones/data/ocupaciones/mappers/EmpleadoMapper.kt
+++ app/src/main/java/edu/ucne/registroocupaciones/data/ocupaciones/mappers/EmpleadoMapper.kt
@@ -3,7 +3,7 @@ package edu.ucne.registroocupaciones.data.ocupaciones.mappers
 import edu.ucne.registroocupaciones.data.ocupaciones.local.entities.EmpleadoEntity
 import edu.ucne.registroocupaciones.domain.empleados.model.Empleado
 
-fun Empleado.toDomain () : Empleado = Empleado (
+fun EmpleadoEntity.toDomain () : Empleado = Empleado (
     empleadoId = empleadoId,
     fechaIngreso = fechaIngreso,
     nombres = nombres,
