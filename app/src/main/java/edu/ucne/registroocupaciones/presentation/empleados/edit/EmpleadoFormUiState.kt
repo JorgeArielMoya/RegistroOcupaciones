42bacb4246d2a40ff266bf23e968c095302bf2b0
jorge <jorge@LAPTOP-D8GDLQRL>
2026-05-16 13:28:37 -0400

    Trabaja las pantallas de edit del modelo empleado

--- app/src/main/java/edu/ucne/registroocupaciones/presentation/empleados/edit/EmpleadoFormUiState.kt
+++ app/src/main/java/edu/ucne/registroocupaciones/presentation/empleados/edit/EmpleadoFormUiState.kt
@@ -1,4 +1,18 @@
 package edu.ucne.registroocupaciones.presentation.empleados.edit
 
-class EmpleadoFormUiState {
-}
+data class EmpleadoFormUiState(
+    val empleadoId: Int? = null,
+    val nombres: String = "",
+    val sexo: String = "",
+    val fechaIngreso: String = "",
+    val sueldo: String = "",
+    val nombresError: String? = null,
+    val sexoError: String? = null,
+    val fechaIngresoError: String? = null,
+    val sueldoError: String? = null,
+    val isSaving: Boolean = false,
+    val isDeleting: Boolean = false,
+    val isNew: Boolean = true,
+    val saved: Boolean = false,
+    val deleted: Boolean = false
+)
