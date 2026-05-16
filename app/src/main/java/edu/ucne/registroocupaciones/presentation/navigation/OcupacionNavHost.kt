27d13c3c15b8819cb723b07dd740283ad747b894
jorge <jorge@LAPTOP-D8GDLQRL>
2026-05-16 13:30:07 -0400

    Agrega navegacion necesaria para utilizar las pantallas del empleado

--- app/src/main/java/edu/ucne/registroocupaciones/presentation/navigation/OcupacionNavHost.kt
+++ app/src/main/java/edu/ucne/registroocupaciones/presentation/navigation/OcupacionNavHost.kt
@@ -1,13 +1,18 @@
 package edu.ucne.registroocupaciones.presentation.navigation
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import edu.ucne.registroocupaciones.presentation.empleados.edit.EmpleadoFormScreen
+import edu.ucne.registroocupaciones.presentation.empleados.list.EmpleadoListScreen
 import edu.ucne.registroocupaciones.presentation.ocupaciones.edit.OcupacionFormScreen
 import edu.ucne.registroocupaciones.presentation.ocupaciones.list.OcupacionListScreen
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun OcupacionNavHost(
     navController: NavHostController = rememberNavController()
@@ -34,6 +39,27 @@ fun OcupacionNavHost(
                         popUpTo(Screen.OcupacionList) {
                             inclusive = true
                         }
+                    }
+                }
+            )
+        }
+
+        composable<Screen.EmpleadoList> {
+            EmpleadoListScreen(
+                onAddEmpleado = {
+                    navController.navigate(Screen.EmpleadoForm(0))
+                },
+                onNavigateToEdit = { id ->
+                    navController.navigate(Screen.EmpleadoForm(id))
+                }
+            )
+        }
+
+        composable<Screen.EmpleadoForm> {
+            EmpleadoFormScreen(
+                onBack = {
+                    navController.navigate(Screen.EmpleadoList) {
+                        popUpTo(Screen.EmpleadoList) { inclusive = true }
                     }
                 }
             )
