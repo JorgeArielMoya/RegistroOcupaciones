9d726c326f30edb6dfe1a3fc0e1d6c6dd05151e6
jorge <jorge@LAPTOP-D8GDLQRL>
2026-05-15 22:30:09 -0400

    Trabaja los metodos de empleadoDao

---
+++ app/src/main/java/edu/ucne/registroocupaciones/data/ocupaciones/local/dao/EmpleadoDao.kt
@@ -0,0 +1,26 @@
+package edu.ucne.registroocupaciones.data.ocupaciones.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.registroocupaciones.data.ocupaciones.local.entities.EmpleadoEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface EmpleadoDao {
+    @Upsert
+    suspend fun upsert (entity : EmpleadoEntity)
+
+    @Delete
+    suspend fun delete (entity: EmpleadoEntity)
+
+    @Query("Select * from empleados order by empleadoId desc")
+    fun observeAll () : Flow<List<EmpleadoEntity>>
+
+    @Query("Select * from empleados where empleadoId = :id")
+    suspend fun getById (id: Int) : EmpleadoEntity?
+
+    @Query("Delete from empleados where empleadoId = :id")
+    suspend fun deleteById (id : Int)
+}
