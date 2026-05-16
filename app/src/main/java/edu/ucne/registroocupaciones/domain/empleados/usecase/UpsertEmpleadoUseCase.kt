37b4666f74a2893227eeb178d5430147d77d00f6
jorge <jorge@LAPTOP-D8GDLQRL>
2026-05-16 12:34:12 -0400

    Trabaja el upsert del empleado llamando las respectivas validaciones

--- app/src/main/java/edu/ucne/registroocupaciones/domain/empleados/usecase/UpsertEmpleadoUseCase.kt
+++ app/src/main/java/edu/ucne/registroocupaciones/domain/empleados/usecase/UpsertEmpleadoUseCase.kt
@@ -1,4 +1,26 @@
 package edu.ucne.registroocupaciones.domain.empleados.usecase
 
-class UpsertEmpleadoUseCase {
+import edu.ucne.registroocupaciones.domain.empleados.model.Empleado
+import edu.ucne.registroocupaciones.domain.empleados.repository.EmpleadoRepository
+import javax.inject.Inject
+
+class UpsertEmpleadoUseCase @Inject constructor(
+    private val repository: EmpleadoRepository
+) {
+    suspend operator fun invoke(empleado: Empleado): Result<Int> {
+
+        val fechaResult = validateFechaIngreso(empleado.fechaIngreso)
+        if (!fechaResult.isValid)
+            return Result.failure(IllegalArgumentException(fechaResult.error))
+
+        val nombresResult = validateNombres(empleado.nombres)
+        if (!nombresResult.isValid)
+            return Result.failure(IllegalArgumentException(nombresResult.error))
+
+        val sueldoResult = validateSueldo(empleado.sueldo.toString())
+        if (!sueldoResult.isValid)
+            return Result.failure(IllegalArgumentException(sueldoResult.error))
+
+        return runCatching { repository.upsert(empleado) }
+    }
 }
