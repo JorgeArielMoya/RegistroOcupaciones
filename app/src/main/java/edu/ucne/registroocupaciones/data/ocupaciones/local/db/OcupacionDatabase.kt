0a1dd821880588ce29320091a3f193fddc691254
jorge <jorge@LAPTOP-D8GDLQRL>
2026-05-15 22:31:40 -0400

    Actualiza la base de datos y agrega el metodo empleado dao a la base de datos

--- app/src/main/java/edu/ucne/registroocupaciones/data/ocupaciones/local/db/OcupacionDatabase.kt
+++ app/src/main/java/edu/ucne/registroocupaciones/data/ocupaciones/local/db/OcupacionDatabase.kt
@@ -2,15 +2,18 @@ package edu.ucne.registroocupaciones.data.ocupaciones.local.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import edu.ucne.registroocupaciones.data.ocupaciones.local.dao.EmpleadoDao
 import edu.ucne.registroocupaciones.data.ocupaciones.local.dao.OcupacionDao
+import edu.ucne.registroocupaciones.data.ocupaciones.local.entities.EmpleadoEntity
 import edu.ucne.registroocupaciones.data.ocupaciones.local.entities.OcupacionEntity
 
 @Database(
-    entities = [OcupacionEntity::class],
-    version = 1,
+    entities = [OcupacionEntity::class, EmpleadoEntity::class],
+    version = 2,
     exportSchema = false
 )
 
 abstract class OcupacionDatabase : RoomDatabase() {
     abstract fun ocupacionDao () : OcupacionDao
+    abstract fun empleadoDao () : EmpleadoDao
 }
