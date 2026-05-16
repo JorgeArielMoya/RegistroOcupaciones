984db27ca5700aedf2fefbc7510c2f2cccc081eb
jorge <jorge@LAPTOP-D8GDLQRL>
2026-05-16 12:26:54 -0400

    Trabaja el delete use case

--- app/src/main/java/edu/ucne/registroocupaciones/domain/empleados/usecase/DeleteEmpleadoUseCase.kt
+++ app/src/main/java/edu/ucne/registroocupaciones/domain/empleados/usecase/DeleteEmpleadoUseCase.kt
@@ -1,4 +1,10 @@
 package edu.ucne.registroocupaciones.domain.empleados.usecase
 
-class DeleteEmpleadoUseCase {
+import edu.ucne.registroocupaciones.domain.empleados.repository.EmpleadoRepository
+import javax.inject.Inject
+
+class DeleteEmpleadoUseCase @Inject constructor(
+    private val repository: EmpleadoRepository
+) {
+    suspend operator fun invoke(id : Int) = repository.delete(id)
 }
