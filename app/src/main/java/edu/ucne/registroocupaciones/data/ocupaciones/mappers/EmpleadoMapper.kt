31effa10c0161c57de3ec04dfb3894db4f2d933d
jorge <jorge@LAPTOP-D8GDLQRL>
2026-05-15 22:49:00 -0400

    Agrega el metodo todomain y el metodo toEntity a la clase empleado

---
+++ app/src/main/java/edu/ucne/registroocupaciones/data/ocupaciones/mappers/EmpleadoMapper.kt
@@ -0,0 +1,20 @@
+package edu.ucne.registroocupaciones.data.ocupaciones.mappers
+
+import edu.ucne.registroocupaciones.data.ocupaciones.local.entities.EmpleadoEntity
+import edu.ucne.registroocupaciones.domain.empleados.model.Empleado
+
+fun Empleado.toDomain () : Empleado = Empleado (
+    empleadoId = empleadoId,
+    fechaIngreso = fechaIngreso,
+    nombres = nombres,
+    sexo = sexo,
+    sueldo = sueldo
+)
+
+fun Empleado.toEntity () : EmpleadoEntity = EmpleadoEntity (
+    empleadoId = empleadoId,
+    fechaIngreso = fechaIngreso,
+    nombres = nombres,
+    sexo = sexo,
+    sueldo = sueldo
+)
