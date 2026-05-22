063b017328dd84bf3d5e45a14496cbb0fdc78a4b
jorge <jorge@LAPTOP-D8GDLQRL>
2026-05-22 12:10:45 -0400

    Agrega '@AndroidEntryPoint' para arreglar el crasheo de la aplicacion

--- app/src/main/java/edu/ucne/registroocupaciones/MainActivity.kt
+++ app/src/main/java/edu/ucne/registroocupaciones/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.registroocupaciones.presentation.navigation.AppNavHost
 import edu.ucne.registroocupaciones.ui.theme.RegistroOcupacionesTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
