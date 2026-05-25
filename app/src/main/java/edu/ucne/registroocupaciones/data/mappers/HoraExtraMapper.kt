2001224b8c7fe57b299f8825446766d371f435ba
jorge <jorge@LAPTOP-D8GDLQRL>
2026-05-25 16:32:13 -0400

    Trabaja Hora extra mapper

--- app/src/main/java/edu/ucne/registroocupaciones/data/mappers/HoraExtraMapper.kt
+++ app/src/main/java/edu/ucne/registroocupaciones/data/mappers/HoraExtraMapper.kt
@@ -1,2 +1,22 @@
 package edu.ucne.registroocupaciones.data.mappers
 
+import edu.ucne.registroocupaciones.data.local.entities.HoraExtraEntity
+import edu.ucne.registroocupaciones.domain.horasextras.model.HoraExtra
+
+fun HoraExtraEntity.toDomain () : HoraExtra = HoraExtra(
+    horaExtraId = horaExtraId,
+    empleadoId = empleadoId,
+    fechaDesde = fechaDesde,
+    fechaHasta = fechaHasta,
+    horasTotales = horasTotales,
+    horasNocturnas = horasNocturnas
+)
+
+fun HoraExtra.toEntity () : HoraExtraEntity = HoraExtraEntity(
+    horaExtraId = horaExtraId,
+    empleadoId = empleadoId,
+    fechaDesde = fechaDesde,
+    fechaHasta = fechaHasta,
+    horasTotales = horasTotales,
+    horasNocturnas = horasNocturnas
+)
