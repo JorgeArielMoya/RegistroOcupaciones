34b40b7fb7ebe481294e592dde9256566e3dd0a0
jorge <jorge@LAPTOP-D8GDLQRL>
2026-05-29 07:27:41 -0400

    Agrega version, libreria y dependencia para las pantallas adaptativas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.material3.adaptive)
+
     // Room
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
